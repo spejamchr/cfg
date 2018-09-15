a07038163e8da9fde05d6d200919a04ab279bec7
Spencer Christiansen <jc.spencer92@gmail.com>
2018-09-14 20:26:02 -0600

    Add qutebrowser config
    I overwrote my own config thinking I already had it in git. Oops. Here's
    a new one.

---
+++ .qutebrowser/config.py
@@ -0,0 +1,107 @@
+# general settings
+c.editor.command = ["urxvt", "-title", "scratchpad", "-geometry", "86x24+40+60", "-e", "nvim", "-f", "{}"]
+c.new_instance_open_target = "tab-bg"
+c.prompt.filebrowser = False
+c.completion.height = "30%"
+c.completion.web_history_max_items = 1000
+c.input.partial_timeout = 2000
+c.tabs.background = True
+c.tabs.favicons.show = "always"
+c.tabs.title.format = "{title}"
+c.tabs.position = "left"
+c.tabs.width = 100
+c.downloads.location.directory = '~/Downloads'
+c.content.cache.size = 52428800
+c.content.webgl = False
+c.content.host_blocking.enabled = True
+c.hints.border = "1px solid #CCCCCC"
+c.hints.mode = "letter"
+c.hints.min_chars = 1
+c.hints.uppercase = True
+c.keyhint.blacklist = ["*"]
+c.scrolling.bar = True
+c.scrolling.smooth = True
+
+# searches
+c.url.searchengines['DEFAULT'] = 'https://searx.me/?q={}'
+c.url.searchengines['a'] = 'https://wiki.archlinux.org/?search={}'
+c.url.searchengines['d'] = 'https://duckduckgo.com/?q={}'
+c.url.searchengines['g'] = 'http://www.google.com/search?hl=en&source=hp&ie=ISO-8859-l&q={}'
+c.url.searchengines['y'] = 'https://www.youtube.com/results?search_query={}'
+c.url.searchengines['w'] = 'https://secure.wikimedia.org/wikipedia/en/w/index.php?title=Special%%3ASearch&search={}'
+c.url.searchengines['gh'] = 'https://github.com/search?q={}&type=Code'
+
+# colors
+c.colors.completion.fg = "#899CA1"
+c.colors.completion.category.fg = "#F2F2F2"
+c.colors.completion.category.bg = "#555555"
+c.colors.completion.item.selected.fg = "white"
+c.colors.completion.item.selected.bg = "#333333"
+c.colors.completion.item.selected.border.top = "#333333"
+c.colors.completion.item.selected.border.bottom = "#333333"
+c.colors.completion.match.fg = "#F2F2F2"
+c.colors.statusbar.normal.fg = "#899CA1"
+c.colors.statusbar.normal.bg = "#222222"
+c.colors.statusbar.insert.fg = "#899CA1"
+c.colors.statusbar.insert.bg = "#222222"
+c.colors.statusbar.command.bg = "#555555"
+c.colors.statusbar.command.fg = "#F0F0F0"
+c.colors.statusbar.caret.bg = "#5E468C"
+c.colors.statusbar.caret.selection.fg = "white"
+c.colors.statusbar.progress.bg = "#333333"
+c.colors.statusbar.passthrough.bg = "#4779B3"
+c.colors.statusbar.url.fg = c.colors.statusbar.normal.fg
+c.colors.statusbar.url.success.http.fg = "#899CA1"
+c.colors.statusbar.url.success.https.fg = "#53A6A6"
+c.colors.statusbar.url.error.fg = "#8A2F58"
+c.colors.statusbar.url.warn.fg = "#914E89"
+c.colors.statusbar.url.hover.fg = "#2B7694"
+c.colors.tabs.bar.bg = "#222222"
+c.colors.tabs.even.fg = "#899CA1"
+c.colors.tabs.even.bg = "#222222"
+c.colors.tabs.odd.fg = "#899CA1"
+c.colors.tabs.odd.bg = "#222222"
+c.colors.tabs.selected.even.fg = "white"
+c.colors.tabs.selected.even.bg = "#222222"
+c.colors.tabs.selected.odd.fg = "white"
+c.colors.tabs.selected.odd.bg = "#222222"
+c.colors.tabs.indicator.start = "#4444ff"
+c.colors.tabs.indicator.stop = "#222222"
+c.colors.tabs.indicator.error = "#8A2F58"
+c.colors.hints.bg = "#CCCCCC"
+c.colors.hints.match.fg = "#000"
+c.colors.downloads.start.fg = "black"
+c.colors.downloads.start.bg = "#BFBFBF"
+c.colors.downloads.stop.fg = "black"
+c.colors.downloads.stop.bg = "#F0F0F0"
+c.colors.keyhint.fg = "#FFFFFF"
+c.colors.keyhint.suffix.fg = "#FFFF00"
+c.colors.keyhint.bg = "rgba(0, 0, 0, 80%)"
+c.colors.messages.error.bg = "#8A2F58"
+c.colors.messages.error.border = "#8A2F58"
+c.colors.messages.warning.bg = "#BF85CC"
+c.colors.messages.warning.border = c.colors.messages.warning.bg
+c.colors.messages.info.bg = "#333333"
+c.colors.prompts.fg = "#333333"
+c.colors.prompts.bg = "#DDDDDD"
+c.colors.prompts.selected.bg = "#4779B3"
+
+# fonts
+c.fonts.monospace = '"Fira Code","xos4 Terminus", Terminus, Monospace, "DejaVu Sans Mono", Monaco, "Bitstream Vera Sans Mono", "Andale Mono", "Courier New", Courier, "Liberation Mono", monospace, Fixed, Consolas, Terminal'
+c.fonts.tabs = "11 monospace"
+c.fonts.statusbar = "12pt monospace"
+c.fonts.downloads = c.fonts.statusbar
+c.fonts.prompts = c.fonts.statusbar
+c.fonts.hints = "bold 11pt monospace"
+c.fonts.messages.info = "12pt monospace"
+c.fonts.keyhint = c.fonts.messages.info
+c.fonts.messages.warning = c.fonts.messages.info
+c.fonts.messages.error = c.fonts.messages.info
+c.fonts.completion.entry = c.fonts.statusbar
+c.fonts.completion.category = c.fonts.statusbar
+
+# keybinds
+# Focus the first visible input on the page
+config.bind('gi', 'enter-mode insert ;; jseval --quiet var inputs = document.getElementsByTagName("input"); for(var i = 0; i < inputs.length; i++) { var hidden = false; for(var j = 0; j < inputs[i].attributes.length; j++) { hidden = hidden || inputs[i].attributes[j].value.includes("hidden"); }; if(!hidden) { inputs[i].focus(); break; } }')
+config.bind('V', 'spawn mpv {url}')
+config.bind('M', 'hint links spawn mpv {hint-url}')
