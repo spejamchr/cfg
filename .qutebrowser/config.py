617d381f18ebe0851798066c03ba72a964c943df
Spencer Christiansen <jc.spencer92@gmail.com>
2019-02-27 00:54:15 -0700

    qute: Dracula theme!
    I tried using [`solarized-everything-css`](sol) to generate
    dracula-themed user stylesheets, and it turned out pretty good for
    Github and Wikipedia, but it messed up the styles for ahgconnect pretty
    bad. So I had to dump that. :/
    
    [sol]: https://github.com/alphapapa/solarized-everything-css

--- .qutebrowser/config.py
+++ .qutebrowser/config.py
@@ -1,3 +1,20 @@
+# Load ~/Library/Preferences/qutebrowser/autoconfig.yml
+# See: https://qutebrowser.org/doc/help/configuring.html#configpy-autoconfig
+config.load_autoconfig()
+
+# From https://github.com/evannagle/qutebrowser-dracula-theme
+import dracula.draw
+dracula.draw.blood(c, {
+    'spacing': {
+        'vertical': 0,
+        'horizontal': 5
+    },
+    'font': {
+        'family': '"Fira Code", Menlo, Terminus, Monaco, Monospace',
+        'size': 12
+    }
+})
+
 # general settings
 c.editor.command = ["kitty", "nvim", "{file}", "+norm {line}G{column}|"]
 c.new_instance_open_target = "tab-bg"
@@ -34,75 +51,6 @@ c.url.searchengines['g'] = 'http://www.google.com/search?hl=en&source=hp&ie=ISO-
 c.url.searchengines['y'] = 'https://www.youtube.com/results?search_query={}'
 c.url.searchengines['w'] = 'https://secure.wikimedia.org/wikipedia/en/w/index.php?title=Special%%3ASearch&search={}'
 c.url.searchengines['gh'] = 'https://github.com/search?q={}&type=Code'
-
-# colors
-c.colors.completion.fg = "#899CA1"
-c.colors.completion.category.fg = "#F2F2F2"
-c.colors.completion.category.bg = "#555555"
-c.colors.completion.item.selected.fg = "white"
-c.colors.completion.item.selected.bg = "#333333"
-c.colors.completion.item.selected.border.top = "#333333"
-c.colors.completion.item.selected.border.bottom = "#333333"
-c.colors.completion.match.fg = "#F2F2F2"
-c.colors.statusbar.normal.fg = "#899CA1"
-c.colors.statusbar.normal.bg = "#222222"
-c.colors.statusbar.insert.fg = "#899CA1"
-c.colors.statusbar.insert.bg = "#222222"
-c.colors.statusbar.command.bg = "#555555"
-c.colors.statusbar.command.fg = "#F0F0F0"
-c.colors.statusbar.caret.bg = "#5E468C"
-c.colors.statusbar.caret.selection.fg = "white"
-c.colors.statusbar.progress.bg = "#333333"
-c.colors.statusbar.passthrough.bg = "#4779B3"
-c.colors.statusbar.url.fg = c.colors.statusbar.normal.fg
-c.colors.statusbar.url.success.http.fg = "#899CA1"
-c.colors.statusbar.url.success.https.fg = "#53A6A6"
-c.colors.statusbar.url.error.fg = "#8A2F58"
-c.colors.statusbar.url.warn.fg = "#914E89"
-c.colors.statusbar.url.hover.fg = "#2B7694"
-c.colors.tabs.bar.bg = "#222222"
-c.colors.tabs.even.fg = "#899CA1"
-c.colors.tabs.even.bg = "#222222"
-c.colors.tabs.odd.fg = "#899CA1"
-c.colors.tabs.odd.bg = "#222222"
-c.colors.tabs.selected.even.fg = "white"
-c.colors.tabs.selected.even.bg = "#222222"
-c.colors.tabs.selected.odd.fg = "white"
-c.colors.tabs.selected.odd.bg = "#222222"
-c.colors.tabs.indicator.start = "#4444ff"
-c.colors.tabs.indicator.stop = "#222222"
-c.colors.tabs.indicator.error = "#8A2F58"
-c.colors.hints.bg = "#CCCCCC"
-c.colors.hints.match.fg = "#000"
-c.colors.downloads.start.fg = "black"
-c.colors.downloads.start.bg = "#BFBFBF"
-c.colors.downloads.stop.fg = "black"
-c.colors.downloads.stop.bg = "#F0F0F0"
-c.colors.keyhint.fg = "#FFFFFF"
-c.colors.keyhint.suffix.fg = "#FFFF00"
-c.colors.keyhint.bg = "rgba(0, 0, 0, 80%)"
-c.colors.messages.error.bg = "#8A2F58"
-c.colors.messages.error.border = "#8A2F58"
-c.colors.messages.warning.bg = "#BF85CC"
-c.colors.messages.warning.border = c.colors.messages.warning.bg
-c.colors.messages.info.bg = "#333333"
-c.colors.prompts.fg = "#333333"
-c.colors.prompts.bg = "#DDDDDD"
-c.colors.prompts.selected.bg = "#4779B3"
-
-# fonts
-c.fonts.monospace = '"Fira Code","xos4 Terminus", Terminus, Monospace, "DejaVu Sans Mono", Monaco, "Bitstream Vera Sans Mono", "Andale Mono", "Courier New", Courier, "Liberation Mono", monospace, Fixed, Consolas, Terminal'
-c.fonts.tabs = "11 monospace"
-c.fonts.statusbar = "12pt monospace"
-c.fonts.downloads = c.fonts.statusbar
-c.fonts.prompts = c.fonts.statusbar
-c.fonts.hints = "bold 11pt monospace"
-c.fonts.messages.info = "12pt monospace"
-c.fonts.keyhint = c.fonts.messages.info
-c.fonts.messages.warning = c.fonts.messages.info
-c.fonts.messages.error = c.fonts.messages.info
-c.fonts.completion.entry = c.fonts.statusbar
-c.fonts.completion.category = c.fonts.statusbar
 
 # keybinds
 # Focus the first visible input on the page
