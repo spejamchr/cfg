3636580a61231549f74240c869938751fdf95a66
Spencer Christiansen <jc.spencer92@gmail.com>
2019-10-03 22:12:02 -0600

    Make upgrading simpler

---
+++ update.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env zsh
+
+function upgrade_kitty() {
+  echo "-> Upgrading kitty..."
+  local kitty_path="$HOME/git/other/kitty"
+  if [[ -d "$kitty_path" ]]; then
+    (
+      cd "$kitty_path"
+      if [[ $(git fetch) ]]; then
+        git pull
+        make
+      else
+        echo "Already up-to-date\n"
+      fi
+    )
+  else
+    echo "Kitty is not available at $kitty_path\n"
+  fi
+}
+
+function updgrade_zsh_plugins() {
+  echo "-> Upgrading zsh plugins..."
+  source "$HOME/.zshrc"
+  if which zplug >&-; then
+    if zplug check; then
+      echo "Already up-to-date\n"
+    else
+      zplug update
+    fi
+  else
+    echo "zplug is not available\n"
+  fi
+}
+
+function upgrade_brew_stuff() {
+  echo "-> Upgrading brew..."
+  if which brew >&-; then
+    brew update
+    echo "\n-> Upgrading brew packages..."
+    [[ $(brew upgrade) ]] || echo "Already up-to-date\n"
+    echo "-> Upgrading brew casks..."
+    if [[ $(brew cask outdated) ]]; then
+      brew cask upgrade
+    else
+      echo "Already up-to-date\n"
+    fi
+  else
+    echo "brew is not available\n"
+  fi
+}
+
+upgrade_kitty
+updgrade_zsh_plugins
+upgrade_brew_stuff
