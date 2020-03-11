88b174d9d06c16b1f5bc0513a5864f692d7fec71
Spencer Christiansen <jc.spencer92@gmail.com>
2020-03-10 23:06:53 -0600

    topbar: Don't render an empty span if there is no current window

--- home/config/ubersicht/widgets/topbar/Components/Window.jsx
+++ home/config/ubersicht/widgets/topbar/Components/Window.jsx
@@ -1,14 +1,9 @@
 import { React } from "uebersicht";
 import Item from "./item.jsx";
 
-const Window = ({ windows }) => (
-  <span>
-    {windows
-      .filter(w => w.focused === 1)
-      .map(w => (
-        <Item key={w.id} text={w.app} />
-      ))}
-  </span>
-);
+const Window = ({ windows }) =>
+  windows
+    .filter(w => w.focused === 1)
+    .map(w => <Item key={w.id} text={w.app} />);
 
 export default Window;
