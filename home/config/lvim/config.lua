4a001b0ab3d8ade9b72edba3934474f8854253c1
Spencer Christiansen <jc.spencer92@gmail.com>
2022-09-13 21:28:53 -0600

    lvim: Initial lvim configuration

--- home/config/lvim/config.lua
+++ home/config/lvim/config.lua
@@ -11,7 +11,7 @@ an executable
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save = true
-lvim.colorscheme = "onedarker"
+-- lvim.colorscheme = "onedarker"
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
 
@@ -19,6 +19,8 @@ lvim.colorscheme = "onedarker"
 lvim.leader = "space"
 -- add your own keymapping
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+lvim.keys.normal_mode["-"] = ":Neotree reveal current dir=%:p:h<CR>"
+lvim.keys.normal_mode["<space>e"] = ":Neotree reveal current dir=%:p:h<CR>"
 -- lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 -- lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 -- unmap a default keymapping
@@ -45,6 +47,7 @@ lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 -- }
 
 -- Use which-key to add extra bindings with the leader-key prefix
+-- lvim.builtin.which_key.mappings["e"] = false
 -- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
 -- lvim.builtin.which_key.mappings["t"] = {
 --   name = "+Trouble",
@@ -61,23 +64,21 @@ lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.notify.active = true
-lvim.builtin.terminal.active = true
-lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+lvim.builtin.nvimtree.active = false
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
   "bash",
-  "c",
+  -- "c",
   "javascript",
   "json",
   "lua",
-  "python",
+  -- "python",
   "typescript",
   "tsx",
   "css",
   "rust",
-  "java",
+  -- "java",
   "yaml",
 }
 
@@ -160,13 +161,117 @@ lvim.builtin.treesitter.highlight.enabled = true
 -- }
 
 -- Additional Plugins
--- lvim.plugins = {
---     {"folke/tokyonight.nvim"},
---     {
---       "folke/trouble.nvim",
---       cmd = "TroubleToggle",
---     },
--- }
+lvim.plugins = {
+  { "tpope/vim-repeat" },
+  {
+    "tpope/vim-surround",
+    -- make sure to change the value of `timeoutlen` if it's not triggering correctly, see https://github.com/tpope/vim-surround/issues/117
+    -- setup = function()
+    --  vim.o.timeoutlen = 500
+    -- end
+  },
+  { "ggandor/leap.nvim", config = function()
+    require('leap').set_default_keymaps()
+  end, },
+  {
+    "tpope/vim-fugitive",
+    cmd = {
+      "G",
+      "Git",
+      "Gdiffsplit",
+      "Gread",
+      "Gwrite",
+      "Ggrep",
+      "GMove",
+      "GDelete",
+      "GBrowse",
+      "GRemove",
+      "GRename",
+      "Glgrep",
+      "Gedit"
+    },
+    ft = { "fugitive" }
+  },
+  {
+    "norcalli/nvim-colorizer.lua",
+    config = function()
+      require("colorizer").setup({ "css", "scss", "html", "javascript" }, {
+        RGB = true, -- #RGB hex codes
+        RRGGBB = true, -- #RRGGBB hex codes
+        RRGGBBAA = true, -- #RRGGBBAA hex codes
+        rgb_fn = true, -- CSS rgb() and rgba() functions
+        hsl_fn = true, -- CSS hsl() and hsla() functions
+        css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+        css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+      })
+    end,
+  },
+  {
+    "folke/lsp-colors.nvim",
+    event = "BufRead",
+  },
+  { "RRethy/nvim-base16" },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v2.x",
+    requires = {
+      "nvim-lua/plenary.nvim",
+      "kyazdani42/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    },
+    config = function()
+      vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+
+
+      local config = {
+        window = {
+          position = "current", -- left, right, top, bottom, float, current
+          mappings = {
+            ["s"] = "open_split",
+            ["v"] = "open_vsplit",
+          },
+        },
+        filesystem = {
+          window = {
+            mappings = {
+              ["-"] = "navigate_up",
+            }
+          },
+          bind_to_cwd = false, -- true creates a 2-way binding between vim's cwd and neo-tree's root
+          cwd_target = {
+            sidebar = "tab", -- sidebar is when position = left or right
+            current = "window" -- current is when position = current
+          },
+          -- The renderer section provides the renderers that will be used to render the tree.
+          --   The first level is the node type.
+          --   For each node type, you can specify a list of components to render.
+          --       Components are rendered in the order they are specified.
+          --         The first field in each component is the name of the function to call.
+          --         The rest of the fields are passed to the function as the "config" argument.
+          filtered_items = {
+            hide_gitignored = false,
+          },
+          hijack_netrw_behavior = "open_current", -- netrw disabled, opening a directory opens neo-tree
+          -- in whatever position is specified in window.position
+          -- "open_current",-- netrw disabled, opening a directory opens within the
+          -- window like netrw would, regardless of window.position
+          -- "disabled",    -- netrw left alone, neo-tree does not handle opening dirs
+        },
+        buffers = {
+          bind_to_cwd = false,
+          window = {
+            mappings = {
+              ["-"] = "navigate_up",
+            },
+          },
+        },
+      }
+
+
+      require("neo-tree").setup(config)
+    end,
+  },
+}
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 -- vim.api.nvim_create_autocmd("BufEnter", {
@@ -181,3 +286,19 @@ lvim.builtin.treesitter.highlight.enabled = true
 --     require("nvim-treesitter.highlight").attach(0, "bash")
 --   end,
 -- })
+
+vim.opt.colorcolumn = vim.api.nvim_eval('join(range(101, 999), ",")')
+vim.opt.number = false
+vim.opt.wrap = true
+vim.opt.scrolloff = 0
+vim.opt.cmdheight = 1
+
+vim.cmd([[
+if filereadable(expand("~/.vimrc_background"))
+  let base16colorspace=256
+  set termguicolors
+  source ~/.vimrc_background
+endif
+]])
+
+lvim.colorscheme = vim.api.nvim_exec([[ colorscheme ]], true)
