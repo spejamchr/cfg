e4ab1e52d8fa845d39d2e2b274f17ce45cbf0bce
Spencer Christiansen <jc.spencer92@gmail.com>
2022-10-08 22:29:03 -0600

    Only auto-format ts & js files

--- home/config/lvim/config.lua
+++ home/config/lvim/config.lua
@@ -12,7 +12,9 @@ require("base16-colorscheme").with_config { telescope = false }
 
 -- general
 lvim.log.level = "warn"
-lvim.format_on_save = false
+lvim.format_on_save = {
+  pattern = "*.ts,*.tsx,*.js,*.jsx",
+}
 -- lvim.colorscheme = "onedarker"
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
@@ -131,21 +133,14 @@ lvim.builtin.treesitter.highlight.enabled = true
 --   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
 -- end
 
--- -- set a formatter, this will override the language server formatting capabilities (if it exists)
--- local formatters = require "lvim.lsp.null-ls.formatters"
--- formatters.setup {
---   { command = "black", filetypes = { "python" } },
---   { command = "isort", filetypes = { "python" } },
---   {
---     -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
---     command = "prettier",
---     ---@usage arguments to pass to the formatter
---     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
---     extra_args = { "--print-with", "100" },
---     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
---     filetypes = { "typescript", "typescriptreact" },
---   },
--- }
+-- set a formatter, this will override the language server formatting capabilities (if it exists)
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+  {
+    command = "prettier",
+    extra_args = { "--print-with", "100", "--single-quote", "--trailing-comma", "all" },
+  },
+}
 
 -- -- set additional linters
 -- local linters = require "lvim.lsp.null-ls.linters"
@@ -236,6 +231,7 @@ lvim.plugins = {
 
       local config = {
         use_default_mappings = false,
+        use_popups_for_input = false,
         window = {
           position = "current",
         },
@@ -246,8 +242,19 @@ lvim.plugins = {
               ["<cr>"] = "open",
               ["H"] = "toggle_hidden",
               ["r"] = "refresh",
-              ["a"] = "add",
-              ["m"] = "move",
+              ["a"] = {
+                "add",
+                config = {
+                  show_path = "relative",
+                }
+              },
+              ["m"] = {
+                "move",
+                config = {
+                  show_path = "relative",
+                }
+              },
+              ["d"] = "delete",
               ["?"] = "show_help",
             }
           },
@@ -290,6 +297,7 @@ vim.opt.whichwrap = "b,s"
 vim.opt.formatoptions = 'jcroql'
 vim.opt.textwidth = 100
 vim.opt.titlestring = 'lvim'
+vim.opt.showtabline = 1
 vim.api.nvim_del_augroup_by_name("_format_options")
 
 vim.cmd([[
