5cdf067572ae6fb47259510186bae4757396aad7
Spencer Christiansen <jc.spencer92@gmail.com>
2023-03-03 10:01:09 -0700

    work: Start keycloak saml-idp too now

--- home/config/fish/functions/eo_terminal.fish
+++ home/config/fish/functions/eo_terminal.fish
@@ -7,5 +7,6 @@ function eo_terminal
         in_new_kitty_window "P3木" "cd $EO && docker logs -f exec_online-p3-1" &&
         in_new_kitty_window "puma木" "cd $EO && docker logs -f exec_online-exo_app-1" &&
         in_new_kitty_window pianoctl pianoctl &&
-        in_new_kitty_window saml "cd ~/work/saml && saml-idp --acsUrl 'https://dev-973490.okta.com/sso/saml2/0oapck7q3mWB0dStg356' --audience 'https://www.okta.com/saml2/service-provider/spsxqsdhiidkjmaqpwcv'"
+        in_new_kitty_window saml-okta "cd ~/work/saml && saml-idp --acsUrl 'https://dev-973490.okta.com/sso/saml2/0oapck7q3mWB0dStg356' --audience 'https://www.okta.com/saml2/service-provider/spsxqsdhiidkjmaqpwcv'"
+        in_new_kitty_window saml-keycloak "cd ~/work/saml && saml-idp --acsUrl 'https://auth.staging.execonline.com/realms/master/broker/dev-saml-idp/endpoint' --audience 'https://auth.staging.execonline.com/realms/master' --port 7001"
 end
