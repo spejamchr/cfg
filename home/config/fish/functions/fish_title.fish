7438f67b49a6c607bf170fbe0fd9840375d31b04
Spencer Christiansen <jc.spencer92@gmail.com>
2023-03-03 09:59:58 -0700

    env: Update some ENV vars and fix a misspelling

--- home/config/fish/functions/fish_title.fish
+++ home/config/fish/functions/fish_title.fish
@@ -1,4 +1,4 @@
-# Coppied from /usr/local/Cellar/fish/3.3.1/share/fish/functions/fish_title.fish @ line 1
+# Copied from /usr/local/Cellar/fish/3.3.1/share/fish/functions/fish_title.fish @ line 1
 function fish_title
     # emacs' "term" is basically the only term that can't handle it.
     if not set -q INSIDE_EMACS; or string match -vq '*,term:*' -- $INSIDE_EMACS
