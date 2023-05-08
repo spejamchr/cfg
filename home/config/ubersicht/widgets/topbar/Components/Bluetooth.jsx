3994d1c808e2af17902734cdcb501d24ab722de7
Spencer Christiansen <jc.spencer92@gmail.com>
2023-05-07 22:01:23 -0600

    uber: Use standard unicode bluetooth icon

--- home/config/ubersicht/widgets/topbar/Components/Bluetooth.jsx
+++ home/config/ubersicht/widgets/topbar/Components/Bluetooth.jsx
@@ -2,7 +2,7 @@ import { React } from "uebersicht";
 import Item from "./item.jsx";
 
 const Bluetooth = ({ bluetooth, colors }) => {
-  const string = bluetooth.connected ? " ペア" : " オン";
+  const string = bluetooth.connected ? "ペア" : "オン";
 
   const bluetoothColor = bluetooth.connected ? colors.Blue : colors.Green;
 
