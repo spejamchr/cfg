27d96c920f19635c653dc5007e5fdacb1ac9c417
Spencer Christiansen <jc.spencer92@gmail.com>
2023-09-06 09:41:14 -0600

    Use the fish Base16 profile helper

--- home/config/fish/config.fish
+++ home/config/fish/config.fish
@@ -7,16 +7,19 @@ fish_add_path ~/code/scripts
 fish_add_path ~/.bin
 fish_add_path ~/.cargo/bin
 
-if test (arch) = "i386"
-  set HOMEBREW_PREFIX /usr/local
+if test (arch) = i386
+    set HOMEBREW_PREFIX /usr/local
 else
-  set HOMEBREW_PREFIX /opt/homebrew
+    set HOMEBREW_PREFIX /opt/homebrew
 end
 
 fish_add_path -m --path $HOMEBREW_PREFIX/bin
 
 if status is-interactive
     set -gx BASE16_SHELL "$XDG_CONFIG_HOME/base16-shell/"
+    if test -s "$BASE16_SHELL"
+        source "$BASE16_SHELL/profile_helper.fish"
+    end
     set -gx BASE16_SHELL_HOOKS "$XDG_CONFIG_HOME/base16-shell-hooks"
 
     set -g fish_prompt_pwd_dir_length 0
