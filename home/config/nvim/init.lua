2fe2d2596e3bd8d4fc09abdeda0c26f3288e8d7c
Spencer Christiansen <jc.spencer92@gmail.com>
2025-03-29 15:44:09 -0600

    nvim: Switch from Fern -> Oil
    I like the idea of using the familiar vim metaphor for this.

--- home/config/nvim/init.lua
+++ home/config/nvim/init.lua
@@ -72,7 +72,7 @@ end)
 
 -- Keymaps {{{
 vim.keymap.set({ "n" }, "<Leader>w", "<cmd>w<cr><esc>", { desc = "Save File" })
-vim.keymap.set({ "n" }, "-", ":Fern %:h-reveal=%:p<CR>", { desc = "Open Fern" })
+vim.keymap.set({ "n" }, "-", ":Oil<CR>", { desc = "Open File Explorer" })
 vim.keymap.set({ "n" }, "<Leader><space>", function()
   Snacks.picker.files()
 end, { desc = "Find file" })
@@ -101,97 +101,6 @@ end, { desc = "Git Log" })
 vim.keymap.set("n", "<leader>gL", function()
   Snacks.picker.git_log()
 end, { desc = "Git Log (cwd)" })
-
--- Perform 'open' on leaf node, 'expand' on collapsed node, and 'collapse' on
--- expanded node. From fern's docs.
-vim.cmd([[
-nmap <silent><expr>
-      \ <Plug>(fern-action-open-or-expand-or-collapse)
-      \ fern#smart#leaf(
-      \   "\<Plug>(fern-action-open)",
-      \   "\<Plug>(fern-action-expand)",
-      \   "\<Plug>(fern-action-collapse)",
-      \ )
-]])
-
--- Define my Fern mappings
-local function fern_init()
-  -- Splits
-  vim.keymap.set({ "n" }, "<C-x>", "<Plug>(fern-action-open:split)", {
-    buffer = true,
-    desc = "Fern: Open in Split",
-  })
-  vim.keymap.set({ "n" }, "<C-v>", "<Plug>(fern-action-open:vsplit)", {
-    buffer = true,
-    desc = "Fern: Open in Vsplit",
-  })
-
-  -- Navigation
-  vim.keymap.set({ "n" }, "-", "<Plug>(fern-action-leave)", {
-    buffer = true,
-    desc = "Fern: Navigate up the Filesystem",
-  })
-  vim.keymap.set({ "n" }, "_", "<Plug>(fern-action-enter)", {
-    buffer = true,
-    desc = "Fern: Enter the directory",
-  })
-  vim.keymap.set({ "n" }, "r", "<Plug>(fern-action-reload)", {
-    buffer = true,
-    desc = "Fern: Reload the UI",
-  })
-
-  vim.keymap.set(
-    { "n" },
-    "<Return>",
-    "<Plug>(fern-action-open-or-expand-or-collapse)",
-    {
-      buffer = true,
-      desc = "Fern: Open/Expand/Collapse selected",
-    }
-  )
-
-  -- CRUD
-  vim.keymap.set({ "n" }, "<Leader>n", "<Plug>(fern-action-new-path)", {
-    buffer = true,
-    desc = "Fern: Create new file/dir",
-  })
-  vim.keymap.set({ "n" }, "<Leader>d", "<Plug>(fern-action-remove)", {
-    buffer = true,
-    desc = "Fern: Remove selected",
-  })
-  vim.keymap.set({ "n" }, "<Leader>m", "<Plug>(fern-action-rename)", {
-    buffer = true,
-    desc = "Fern: Move selected",
-  })
-  vim.keymap.set({ "n" }, "<Leader>c", "<Plug>(fern-action-copy)", {
-    buffer = true,
-    desc = "Fern: Copy selected",
-  })
-
-  -- Marks
-  vim.keymap.set({ "n" }, "<C-n>", "<Plug>(fern-action-mark:toggle)j", {
-    buffer = true,
-    desc = "Fern: Toggle selection of current and move down",
-  })
-  vim.keymap.set({ "n" }, "<C-p>", "<Plug>(fern-action-mark:toggle)k", {
-    buffer = true,
-    desc = "Fern: Toggle selection of current and move up",
-  })
-  vim.keymap.set({ "n" }, "<C-t>", "<Plug>(fern-action-mark:toggle)", {
-    buffer = true,
-    desc = "Fern: Toggle selection of current",
-  })
-  vim.keymap.set({ "n" }, "<C-c>", "<Plug>(fern-action-mark:clear)", {
-    buffer = true,
-    desc = "Fern: Clear all selections",
-  })
-end
-
-vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "fern" },
-  group = vim.api.nvim_create_augroup("SJC-fern-custom", { clear = true }),
-  callback = fern_init,
-})
 -- }}}
 
 -- Autocmds {{{
@@ -274,6 +183,19 @@ require("lazy").setup({
           lualine_z = {},
         },
       },
+    },
+    {
+      "stevearc/oil.nvim",
+      ---@module 'oil'
+      ---@type oil.SetupOpts
+      opts = {
+        watch_for_changes = true,
+        keymaps = {
+          ["<C-x>"] = { "actions.select", opts = { horizontal = true } },
+          ["<C-v>"] = { "actions.select", opts = { vertical = true } },
+        },
+      },
+      lazy = false,
     },
     {
       "echasnovski/mini.surround",
@@ -403,22 +325,6 @@ require("lazy").setup({
       end,
     },
     { "tpope/vim-fugitive" },
-    {
-      "lambdalisue/vim-fern",
-      init = function()
-        -- Show hidden files by default
-        vim.g["fern#default_hidden"] = 1
-
-        -- Only use my mappings
-        vim.g["fern#disable_default_mappings"] = 1
-
-        vim.g["fern#renderer#default#leaf_symbol"] = "| "
-        vim.g["fern#renderer#default#collapsed_symbol"] = "+ "
-        vim.g["fern#renderer#default#expanded_symbol"] = "- "
-      end,
-    },
-    { "lambdalisue/vim-fern-git-status" },
-    { "lambdalisue/vim-fern-hijack" },
     {
       "lewis6991/gitsigns.nvim",
       opts = {
