894e93b0dc101e15410ca7b710748384e258d2aa
Spencer Christiansen <jc.spencer92@gmail.com>
2026-01-06 13:32:18 -0700

    ubersicht: Handle loved songs in pianobar

--- home/config/ubersicht/widgets/topbar/Components/Pianobar.jsx
+++ home/config/ubersicht/widgets/topbar/Components/Pianobar.jsx
@@ -12,13 +12,18 @@ const songDetails = (song, colors) => {
 
   const title = song.match("(^.*)\\[0m by \\[34m")[1];
   const by = song.match("\\[0m by \\[34m(.*)\\[0m on \\[4m")[1];
-  const on = song.match("\\[0m on \\[4m(.*$)")[1];
+  const on = song.match("\\[0m on \\[4m([^\\[]*)")[1];
+  const loveMatch = song.match(
+    "\\[0m on \\[4m([^\\[]*)\\[0m\\[30m \\[31m(.*)\\[0m\\[0m$",
+  );
+  const loveIcon = loveMatch ? loveMatch[2] : null;
 
   return (
     <>
       <span style={{ color: colors.Green }}>{title}</span> by{" "}
       <span style={{ color: colors.Blue }}>{by}</span> on{" "}
       <span style={{ textDecoration: "underline" }}>{on}</span>
+      <span style={{ color: colors.Red }}> {loveIcon}</span>
     </>
   );
 };
